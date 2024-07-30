f387174ebae9a5e6f6be821aaa6a8f5f276981c1
jimmybui <jimmybuidev@gmail.com>
2024-07-30 06:59:24 -0700

    resize comps and preview image

--- components/PicUi/UploadImage.tsx
+++ components/PicUi/UploadImage.tsx
@@ -28,23 +28,29 @@ export const UploadImage: React.FC<UploadImageProps> = ({setQuestion}) => {
     };
 
     return(
-        <div className= ''>
-            <div className="flex items-center space-x-2">
-                <input
-                id="imageFile"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
-                />
+        <div className= 'flex flex-row gap-2 h-[33%]'>
+            <div className='w-[57%]'>
+                <div className="flex items-center space-x-2">
+                    <input
+                    id="imageFile"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="p-1 border border-gray-300 border-l-0 border-t-0 rounded-md focus:outline-none focus:ring-blue-500 focus:ring-opacity-50"
+                    />
+                </div>
+
+                <button
+                    onClick={() => readPic(src, setQuestion)}
+                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none mt-2 mb-1 focus:ring-blue-600"
+                    >
+                    Get Text inside Image
+                </button>
             </div>
 
-            <button
-                onClick={() => readPic(src, setQuestion)}
-                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none mt-2 mb-1 focus:ring-blue-600"
-                >
-                Generate Text inside Image
-            </button>
+            <div className='w-[42%] border border-gray-300 border-r-0 border-t-0 rounded-md'>
+                {src && <img src={src} alt="Uploaded Preview" className="h-full object-contain mx-auto my-auto" />}
+            </div>
 
         </div>
     )
--- components/PicUi/textComponent.tsx
+++ components/PicUi/textComponent.tsx
@@ -6,7 +6,7 @@ interface TextCompProps {
 
 const TextComp: React.FC<TextCompProps> = ({text }) => {
     return (
-        <div className=" items-center mx-1 h-3/5 pr-2 w-full overflow-scroll rounded-lg">
+        <div className=" items-center mx-1 h-[66%] pr-2 w-full overflow-scroll rounded-lg">
             <div className='pb-4'>
                 {text}
             </div>
