5c75dd638c5e9ee0f17b45c6b833ad7cf2996873
jimmyDev <jimmybuidev@gmail.com>
2024-08-03 19:49:25 -0700

    completed ui

--- components/AnswerUI/AnswerUI.tsx
+++ components/AnswerUI/AnswerUI.tsx
@@ -8,7 +8,7 @@ interface TextCompProps {
 const AnsweUI: React.FC<TextCompProps> = ({answer, isQuestionFocused }) => {
     return (
         <div className={`border-gray-300 rounded-lg border-2 my-4 transition-all duration-500 ${isQuestionFocused ? 'h-1/5' : 'h-2/5'}`}>
-            <div className="items-center mx-1 h-[66%] pr-2 w-full overflow-auto rounded-lg">
+            <div className="items-center mx-1 h-full pr-2 w-full overflow-auto rounded-lg">
             <div className='pb-4'>
                 {answer}
             </div>
